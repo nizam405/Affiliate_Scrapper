9a5e5102c04aebeab6ce425a64e0d2425c1a2251
Nizam <nizammnu@yahoo.com>
2022-01-18 21:58:06 +0600

    changed to prod and post_list template

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seo_tools.settings.dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seo_tools.settings.prod')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
